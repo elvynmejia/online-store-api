b362c55c725a13f86f867e97a9e53d36d4228d18
elvynmejia <elvynmejia@gmail.com>
2015-08-26 15:03:08 -0700

    Adds users firsts specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    #Add corresponding user attributes
+    email { FFaker::Internet.email }
+    password "12345678"
+    password_confirmation "12345678"
   end
 
 end
