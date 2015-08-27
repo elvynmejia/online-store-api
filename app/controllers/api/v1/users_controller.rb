214386bff24c9b0fdb4669f23b1757bacade404c
elvynmejia <elvynmejia@gmail.com>
2015-08-27 00:31:08 -0700

    Adds show action to users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::UsersController < ApplicationController
+	respond_to :json
+	def show
+		respond_with User.find(params[:id])
+	end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     #API versioning, app/controllers/api/v1
     #example: http://api.marketplace.dev/v1/products/1
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do 
+      #Lists resources
+      resources :users, only: :show
     end 
 
   end 
