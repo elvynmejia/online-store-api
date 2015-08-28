48d56cea6c5f19d27ff9a4156edaae7f368d30c0
elvynmejia <elvynmejia@gmail.com>
2015-08-28 13:32:37 -0700

    Refactors the json format param

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-	before(:each) { request.headers['Accept'] = "application/vnd.onlinestore.v1"}
+	before(:each) { 
+		request.headers['Accept'] = "application/vnd.onlinestore.v1, #{Mime::JSON}" }
+
+	before(:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
 
 	describe "GET #show" do 
 		before(:each) do 
 			@user = FactoryGirl.create :user
-			get :show, id: @user.id, format: :json
+			get :show, id: @user.id
 		end
 
 		it "returns the information about a reporter on a hash" do
@@ -22,7 +25,7 @@ describe Api::V1::UsersController do
 		context "when is successfully created" do
 			before(:each) do 
 				@user_attributes = FactoryGirl.attributes_for :user
-				post :create, { user: @user_attributes }, format: :json
+				post :create, { user: @user_attributes }
 			end 
 
 			it "renders the json representation for the user record just created" do
@@ -38,7 +41,7 @@ describe Api::V1::UsersController do
 		 		#Can't create user without an email
 		 		@invalid_user_attributes = { password: "12345678",
 		 									 password_confirmation: "12345678" }
-		 		post :create, { user: @invalid_user_attributes }, format: :json
+		 		post :create, { user: @invalid_user_attributes }
 		 	end 
 
 		 	it "renders an errors json" do
@@ -61,7 +64,7 @@ describe Api::V1::UsersController do
 			before(:each) do 
 				@user = FactoryGirl.create :user
 				patch :update, { id: @user.id, 
-								 user: { email: "update@example.com"} }, format: :json
+								 user: { email: "update@example.com"} }
 			end
 
 			it "renders the json representation for the updated user" do
@@ -76,7 +79,7 @@ describe Api::V1::UsersController do
 			before(:each) do 
 				@user = FactoryGirl.create :user
 				patch :update, { id: @user.id, 
-					             user: { email: "bademail.com" } }, format: :json
+					             user: { email: "bademail.com" } }
 			end
 
 			it "renders an errors json" do
@@ -96,7 +99,7 @@ describe Api::V1::UsersController do
 	describe "DELETE #destroy" do
 		before(:each) do 
 			@user = FactoryGirl.create :user
-			delete :destroy, { id: @user.id }, format: :json
+			delete :destroy, { id: @user.id }
 		end 
 
 		it { should respond_with 204 }
