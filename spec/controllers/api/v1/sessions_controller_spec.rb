e95f046161977f1343a4d88e688fa3641ed6386e
elvynmejia <elvynmejia@gmail.com>
2015-08-29 17:49:48 -0700

    Adds sessions destroy action

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -36,4 +36,15 @@ describe Api::V1::SessionsController do
 			it { should respond_with 422 } 			
 		end
 	end 
+
+	describe "DELETE #destroy" do
+	  
+	  before(:each) do
+	  	@user = FactoryGirl.create :user 
+	  	sign_in @user
+	  	delete :destroy, id: @user.auth_token
+	  end 
+
+	  it { should respond_with 204 }
+	end
 end
